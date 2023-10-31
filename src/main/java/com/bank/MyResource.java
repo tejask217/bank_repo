d0755022e76d52ab5985c03744e4d129dabc5a51
Tejas Kapse <tk2172001@gmail.com>
2023-10-31 16:17:56 +0530

    maven bank project

---
+++ src/main/java/com/bank/MyResource.java
@@ -0,0 +1,35 @@
+package com.bank;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+/**
+ * Root resource (exposed at "myresource" path)
+ */
+@Path("myresource")
+public class MyResource {
+
+    /**
+     * Method handling HTTP GET requests. The returned object will be sent
+     * to the client as "text/plain" media type.
+     *
+     * @return String that will be returned as a text/plain response.
+     */
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getIt() {
+        return "Got it!";
+    }
+    
+    
+    
+    @GET
+    @Path("/hello")
+    public String getMessage()
+    {
+    	
+    	return "This is our first Maven project!!!";
+    }
+}
